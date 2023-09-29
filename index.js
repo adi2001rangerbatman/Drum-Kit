41290390a28860a309fe2f312dab3d1c32425a3c
ASUS <adi2001rangerbatman>
2023-09-30 00:18:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementsByTagName("li")[2].style.color = 'red';
+document.getElementsByTagName("li")[2].innerHTML ="Adithya";
+document.getElementsByClassName("btn")[0].style.backgroundColor ='yellow';
+document.getElementById("title").innerHTML="ADITHYA";
+document.querySelector(".list a").style.color ='red';
+document.querySelector("h1").classList.add("huge");
+
+document.getElementById("title").innerHTML;
